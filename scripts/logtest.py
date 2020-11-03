4f02b89dd1d91b7ad92138f402d36442d4644bca
Frank <escobarbfranklin@gmail.com>
2020-11-02 19:23:42 -0600

    try and catch, file close

--- scripts/logtest.py
+++ scripts/logtest.py
@@ -1,15 +1,20 @@
 import logging
 import sys
 
-pid1stdout = open('/proc/1/fd/1','w')
+try:
+    pid_one_stdout = open('/proc/1/fd/1','w')
+except Exception as e:
+    print(e)
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-handler = logging.StreamHandler(pid1stdout)
+handler = logging.StreamHandler(pid_one_stdout)
 handler.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 logger.info('Log message sent from Python logger script to pid 1 stdout')
+
+pid_one_stdout.close()
